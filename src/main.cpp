833d70d74a0f92bce44ea51f038ba4b84f09842b
John Devine <john.h.devine@gmail.com>
2024-04-02 12:31:47 +0700

    Changed platform.ini to stop displaying junk at start of execution

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
 
 #include <ESP8266WiFi.h>
 
-void setup(){
-  Serial.begin(115200);
+void setup()
+{
+  // Serial.begin(115200);
+  Serial.begin(9600);
+  /* Code below does not work .. spita out lots of garbage
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  */
+
+  // wait for serial port to initialise
+  delay(2000);
   Serial.println();
   Serial.print("ESP8266 Board MAC Address:  ");
   Serial.println(WiFi.macAddress());
+  delay(5000);
 }
- 
-void loop(){
 
+void loop()
+{
+  Serial.print("ESP8266 Board MAC Address(loop):  ");
   Serial.println(WiFi.macAddress());
-  delay(2000);
+  delay(20000);
 }
-
-
- 
